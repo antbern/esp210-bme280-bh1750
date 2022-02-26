b555bfe1d2f97073afa7d8be517513d9e7aebf6e
antbern <40672068+antbern@users.noreply.github.com>
2022-02-26 21:07:30 +0100

    Add TLS and authentication support
    Previously, the connection to the MQTT broker was completely unencrypted.
    This commit fixes that by adding TLS support together with username and
    password authentication for access control. Required is now a TLS
    certificate root CA in PEM format included in the configuration header,
    and the current time is fetched from a global NTP server in order to
    check the certificate validity.
    
    Also improves the interpretation of the messages on the interval topic.

--- ESP210_BMP280_MQTT.ino
+++ ESP210_BMP280_MQTT.ino
@@ -4,13 +4,17 @@
 #include <hp_BH1750.h>
 
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecureBearSSL.h>
 #include <PubSubClient.h>
+
+#include <BearSSLHelpers.h>
+#include <time.h> // NOLINT(modernize-deprecated-headers)
 
 // Wifi and MQTT configuration
 #include "wifi_config.h"
 
 // Wifi and MQTT objects
-WiFiClient esp_client;
+WiFiClientSecure esp_client;
 PubSubClient client(esp_client);
 
 // Sensor objects
@@ -22,17 +26,47 @@ hp_BH1750 bh1750;
 unsigned long lastMsg = 0;
 
 // the default interval between measurements
-unsigned long interval = 5000;
+unsigned long intervalMs = 5000;
 
 
 // the buffer used to format the JSON output to send
 #define MSG_BUFFER_SIZE  (256)
 char msg[MSG_BUFFER_SIZE];
 
-const char* topic_environment = "esp210/environment";
-const char* topic_interval = "esp210/interval";
+
+// the base topic to publish and listen to
+#ifndef TOPIC_BASE
+  #define TOPIC_BASE "esp210"
+#endif
+
+const char* topic_observation =  TOPIC_BASE "/observation";
+const char* topic_interval = TOPIC_BASE "/interval";
 
 const int led = 5;
+
+BearSSL::X509List trusted_roots;
+
+// The correct time is needed to validate the certificate
+// See: https://github.com/cdzombak/esp8266-basic-wifi/blob/d6fba145102ecf68f41a4bf186c5a75e5175d155/
+// and: https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/examples/BearSSL_CertStore/BearSSL_CertStore.ino
+// for source and more explanation.  
+void setClock() {
+    configTime(CFG_TZ, "pool.ntp.org", "time.nist.gov");
+
+    Serial.printf_P(PSTR("%lu: Waiting for NTP time sync "), millis());
+    time_t now = time(nullptr);
+    while (now < 8 * 3600 * 2) {
+        delay(250);
+        Serial.print(".");
+        now = time(nullptr);
+    }
+    Serial.print(F("\r\n"));
+    struct tm timeinfo; // NOLINT(cppcoreguidelines-pro-type-member-init)
+    gmtime_r(&now, &timeinfo);
+    Serial.printf_P(PSTR("Current time (UTC):   %s"), asctime(&timeinfo));
+    localtime_r(&now, &timeinfo);
+    Serial.printf_P(PSTR("Current time (Local): %s"), asctime(&timeinfo));
+}
 
 void setup() {
   // Start Serial
@@ -61,7 +95,8 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
- 
+  setClock();
+
   // Start I2C and connect to sensor  
   Wire.begin(2,14);
   if (!bme.begin(0x76)) {  
@@ -90,11 +125,16 @@ void setup() {
 
   bh1750.calibrateTiming();
   
-  digitalWrite(led, LOW);
+  // configure the WifiClientSecure with the root CA
+  // from https://arduino-esp8266.readthedocs.io/en/3.0.2/esp8266wifi/bearssl-client-secure-class.html
+  trusted_roots.append(mqtt_root_ca);
+  esp_client.setTrustAnchors(&trusted_roots);
 
   // setup MQTT
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
+
+  digitalWrite(led, LOW);
   
 }
 
@@ -105,7 +145,7 @@ void reconnect_mqtt() {
     // Create a random client ID
 
     // Attempt to connect
-    if (client.connect(mqtt_device_name)) {
+    if (client.connect(mqtt_device_name, mqtt_username, mqtt_password)) {
       Serial.println("connected");
 
       // do stuff when connected here (like subscribing to topics)
@@ -130,16 +170,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
 
-  // try to interpret the message as an unsigned long
-  char *end;
-  unsigned long i = strtoul((const char*)payload, &end, 10);
+  // convert topic and payload to strings for easier handling
+  String topicstr(topic);
 
-  if (end != (char*)payload && i >= 500) {
-    // we managed to parse an unsigned long, use it as interval for now
+  // add null-termination in last place (ok as long as we are not near the end of the buffer this points to)
+  payload[length] = '\0';
+  String payloadstr((char*)payload);
+
+  // parse the payload as an integer (long)
+  long i = payloadstr.toInt();
+
+  if (i >= 500) {
+    // we managed to parse a long, use it as interval for now
     Serial.print("New interval received: ");
     Serial.println(i, DEC);
     
-    interval = i;
+    intervalMs = i;
   }
 }
 
@@ -150,7 +196,7 @@ void loop() {
   client.loop();
 
   unsigned long now = millis();
-  if(now - lastMsg > interval) {
+  if(now - lastMsg > intervalMs) {
     lastMsg = now;
 
     // read data: BH1750 first since it takes some time (~500ms)
@@ -167,7 +213,7 @@ void loop() {
     Serial.println(msg);
 
     // publish message
-    client.publish(topic_environment, msg);  
+    client.publish(topic_observation, msg);  
 
     // run auto adjustment of measurement parameters
     bh1750.adjustSettings(80);
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Built for the ESP210 board based on a ESP8266 microcontroller. 
 
-Settings in the Arduino IDE for code compilation and uppload:
+Settings in the Arduino IDE for code compilation and upload:
 * Board: Generic ESP8266 Module
-* Freq: 80 Mhz
+* Freq: 80 Mhz (use 160 MHz for TLS according to https://nofurtherquestions.wordpress.com/2016/03/14/making-an-esp8266-web-accessible/)
 * Flash size: 4MB (any configuration)
 
 Uses libraries:
